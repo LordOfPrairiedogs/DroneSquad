e87cfea736ef3a09d3893362eaf672afe462dabf
Dar DeHaai (home) <lordofprairiedogs@gmail.com>
2016-01-07 22:36:10 -0600

    1/7/2016

--- src/ARDrone/Drone/ArDrone2.java
+++ src/ARDrone/Drone/ArDrone2.java
@@ -1,8 +1,33 @@
 package ARDrone.Drone;
 
+import ARDrone.Utility.ATCommand;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * ARDrone.Utility
  */
 public class ArDrone2 implements Drone
 {
+    private InetAddress inet_addr;
+    private int sequenceNumber;
+
+    public ArDrone2 (String address) throws UnknownHostException {
+        inet_addr =  InetAddress.getByName(address);
+        sequenceNumber = 1;
+    }
+
+    @Override
+    public void setAddress(String address) throws UnknownHostException {
+        inet_addr.getByName(address);
+    }
+
+    @Override
+    public void sendCommand(ATCommand cmd) {
+        //connect to ip
+        //put together string
+        //send string to ip
+        //increment sequenceNumber
+    }
 }
--- src/ARDrone/Drone/Drone.java
+++ src/ARDrone/Drone/Drone.java
@@ -1,8 +1,15 @@
 package ARDrone.Drone;
 
+import ARDrone.Utility.ATCommand;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * ARDrone.Utility
  */
 public interface Drone
 {
+    void setAddress (String address) throws UnknownHostException;
+    void sendCommand(ATCommand cmd);
 }
